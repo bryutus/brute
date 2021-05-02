173f53bc38c41d0f1e4f4d55b074a06c933605da
Ryu Segawa <bryutus@gmail.com>
2021-05-02 17:25:06 +0900

    add create brute feature test

--- main_test.go
+++ main_test.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 
 	"github.com/bryutus/brute/app/infrastructure"
@@ -105,6 +106,84 @@ func TestShowBruteNG(t *testing.T) {
 
 		if response["message"] != test.message {
 			t.Fatalf("Expected response error message %s, got %s", test.message, response["message"])
+		}
+	}
+}
+
+func TestCreateBruteOK(t *testing.T) {
+	testCases := []struct {
+		language_code string
+		phrase        string
+		status        int
+	}{
+		{"is", "þú líka", 201},
+	}
+
+	ts := httptest.NewServer(router())
+	defer ts.Close()
+
+	for _, test := range testCases {
+		args := url.Values{}
+		args.Add("language_code", test.language_code)
+		args.Add("phrase", test.phrase)
+
+		resp, err := http.PostForm(fmt.Sprintf("%s/brute", ts.URL), args)
+		if err != nil {
+			t.Fatalf("Expected no error, got %v", err)
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != test.status {
+			t.Fatalf("Expected status code %d, got %v", test.status, resp.StatusCode)
+		}
+
+		response := getResponse(resp.Body, t)
+
+		if response["language_code"] != test.language_code {
+			t.Fatalf("Expected response language_code %s, got %s", test.language_code, response["language_code"])
+		}
+
+		if response["phrase"] != test.phrase {
+			t.Fatalf("Expected response phrase %s, got %s", test.phrase, response["phrase"])
+		}
+	}
+}
+
+func TestCreateBruteNG(t *testing.T) {
+	testCases := []struct {
+		language_code string
+		phrase        string
+		status        int
+		message       string
+	}{
+		{"", "你也是", 400, "Key: 'requestCreateAphorism.LanguageCode' Error:Field validation for 'LanguageCode' failed on the 'required' tag"},
+		{"zh", "", 400, "Key: 'requestCreateAphorism.Phrase' Error:Field validation for 'Phrase' failed on the 'required' tag"},
+		{"zho", "你也是", 400, "Key: 'requestCreateAphorism.LanguageCode' Error:Field validation for 'LanguageCode' failed on the 'len' tag"},
+		{"is", "þú líka", 400, "Key: 'requestCreateAphorism.LanguageCode' Error:Field validation for 'LanguageCode' failed on the 'exists_language_code' tag"},
+	}
+
+	ts := httptest.NewServer(router())
+	defer ts.Close()
+
+	for _, test := range testCases {
+		args := url.Values{}
+		args.Add("language_code", test.language_code)
+		args.Add("phrase", test.phrase)
+
+		resp, err := http.PostForm(fmt.Sprintf("%s/brute", ts.URL), args)
+		if err != nil {
+			t.Fatalf("Expected no error, got %v", err)
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != test.status {
+			t.Fatalf("Expected status code %d, got %v", test.status, resp.StatusCode)
+		}
+
+		response := getResponse(resp.Body, t)
+
+		if response["message"] != test.message {
+			t.Fatalf("Expected response message %s, got %s", test.message, response["message"])
 		}
 	}
 }
